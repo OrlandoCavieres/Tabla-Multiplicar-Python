dba94261c769b22bec28f2b09f3e724468fa1e77
Orlando Cavieres <frizortake@gmail.com>
2020-07-22 22:54:08 -0400

    Creada funcion esEntero, completado cuerpo de funcion main y corregido formato de funcion crearTabla

--- tablamult.py
+++ tablamult.py
@@ -9,9 +9,35 @@ def crearTabla(numero: int):
     """
     for factor in range(1,11):
         print(f"{numero:^8} X {factor:>2} = {numero*factor}")
+    print()
+
+def esEntero(numero):
+    """
+    Función que verifica que el parámetro número puede o no ser convertido a un
+    entero.
+
+    Args:
+        numero: Entrada que se desea verificar.
+
+    Returns:
+        [Boolean]: True si efectivamente la entrada puede transformarse a entero.
+    """
+    try:
+        numero = int(numero)
+        return True
+    except:
+        return False
 
 def main():
-    pass
+    numeroEntero = False
+    while not numeroEntero:
+        multiplo = input("\nIngrese un número entero para construir su tabla de multiplicar: ")
+        if esEntero(multiplo):
+            numeroEntero = True
+            crearTabla(int(multiplo))
+        else:
+            print("\nIngrese un numero entero")
+
 
 if __name__ == '__main__':
     main()
