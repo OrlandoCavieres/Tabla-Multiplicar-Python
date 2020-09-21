96950bc74223d84d8c8c968a93df4c54963933c3
Orlando Cavieres <frizortake@gmail.com>
2020-09-21 16:49:00 -0300

    Modificaciones al estilo y manejo del estandar PEP8 en variables y funciones.

--- tablamult.py
+++ tablamult.py
@@ -1,42 +1,42 @@
-def crearTabla(numero: int):
+def crear_tabla(numero: int):
     """
     Función que crea una tabla de multiplicar en pantalla con los múltiplos
     del número ingresado desde el 1 al 10.
 
     Args:
-        numero [int]: Número entero del que se quiere realizar la tabla de
-                      multiplicar.
+         numero: Número entero del que se quiere realizar la tabla de multiplicar.
     """
-    for factor in range(1,11):
-        print(f"{numero} X {factor:>2} = {numero*factor}")
+    for factor in range(1, 11):
+        print(f"{numero} X {factor:>2} = {numero * factor}")
     print()
 
-def esEntero(numero):
+
+def es_entero(numero):
     """
     Función que verifica que el parámetro número puede o no ser convertido a un
     entero.
-
     Args:
         numero: Entrada que se desea verificar.
-
     Returns:
-        [Boolean]: True si efectivamente la entrada puede transformarse a entero.
+         True si efectivamente la entrada puede transformarse a entero.
     """
     try:
         numero = int(numero)
         return True
-    except:
+    except ():
         return False
 
+
 def main():
-    numeroEntero = False
-    while not numeroEntero:
+    numero_entero = False
+    while not numero_entero:
         multiplo = input("\nIngrese un número entero para construir su tabla de multiplicar: ")
-        if esEntero(multiplo):
-            numeroEntero = True
-            crearTabla(int(multiplo))
+        if es_entero(multiplo):
+            numero_entero = True
+            crear_tabla(int(multiplo))
         else:
             print("\nIngrese un numero entero")
+
 
 if __name__ == '__main__':
     main()
